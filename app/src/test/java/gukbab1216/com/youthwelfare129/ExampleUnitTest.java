3ca3bfd34a5b37ad1b18ee466ff643dc4edff05d
MUN SEONG UK <bulgogi1216@gmail.com>
2018-08-05 12:43:49 +0900

    Create Basic Project
    and set KR App Title Locale

---
+++ app/src/test/java/gukbab1216/com/youthwelfare129/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package gukbab1216.com.youthwelfare129;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
